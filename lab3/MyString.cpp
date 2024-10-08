a557298c55e813ae4dc47f0819628357cbf5a1ff
andryare1 <andreyyarema.2005@gmail.com>
2024-10-08 22:26:01 +0300

    сделал нерабочую херню

--- lab3/MyString.cpp
+++ lab3/MyString.cpp
@@ -6,6 +6,15 @@ MyString::MyString() {
 	std::cout << "Конструктор без параметров вызван" << std::endl;
 }
 
+MyString::MyString(char* str) {
+	/*this->str = new char[strlen(str) + 1];
+	strcpy(this->str, str);*/
+	this->str = new char[strlen(str) + 1];
+	strcpy(this->str, str);
+
+	std::cout << "Конструктор с параметром вызван" << std::endl;
+}
+
 MyString::~MyString() {
 	delete[] str;
 	std::cout << "Деструктор вызван" << std::endl;
@@ -21,4 +30,5 @@ void MyString::update() {
 
 void MyString::print() {
 	std::cout << str << std::endl;
+	std::cout << "Строка выведена на экран" << std::endl;
 }
--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -1,6 +1,13 @@
 ﻿#include <iostream>
+#include "MyString.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    MyString str1("Строка");
+    MyString* str2 = new MyString("Строка");
+
+    str1.print();
+    str2->print();
+
+    return 0;
 }
